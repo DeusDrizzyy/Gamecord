8e736d853139ffff42f67019e8425d974fbc5ef0
_Dr1zzyx_ <66096216+DeusDrizzyy@users.noreply.github.com>
2026-03-03 18:10:47 -0300

    fix: Minesweeper

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -1,16 +1,13 @@
 const { EmbedBuilder, ActionRowBuilder } = require('discord.js');
-const { disableButtons , getNumEmoji, formatMessage, ButtonBuilder } = require('../utils/utils');
+const { disableButtons, getNumEmoji, formatMessage, ButtonBuilder } = require('../utils/utils');
 const events = require('events');
-
 
 module.exports = class Minesweeper extends events {
   constructor(options = {}) {
-
     if (!options.isSlashGame) options.isSlashGame = false;
     if (!options.message) throw new TypeError('NO_MESSAGE: No message option was provided.');
     if (typeof options.message !== 'object') throw new TypeError('INVALID_MESSAGE: message option must be an object.');
     if (typeof options.isSlashGame !== 'boolean') throw new TypeError('INVALID_COMMAND_TYPE: isSlashGame option must be a boolean.');
-
 
     if (!options.embed) options.embed = {};
     if (!options.embed.title) options.embed.title = 'Minesweeper';
@@ -24,8 +21,8 @@ module.exports = class Minesweeper extends events {
     if (!options.mines) options.mines = 5;
     if (!options.timeoutTime) options.timeoutTime = 60000;
     if (!options.winMessage) options.winMessage = 'You won the Game! You successfully avoided all the mines.';
-    if (!options.loseMessage) options.loseMessage = 'You lost the Game! Beaware of the mines next time.';
-
+    if (!options.loseMessage) options.loseMessage = 'You lost the Game! Beware of the mines next time.';
+    if (!options.timeoutMessage) options.timeoutMessage = 'The game went unanswered for a few minutes and was dropped!';
 
     if (typeof options.embed !== 'object') throw new TypeError('INVALID_EMBED: embed option must be an object.');
     if (typeof options.embed.title !== 'string') throw new TypeError('INVALID_EMBED: embed title must be a string.');
@@ -38,13 +35,13 @@ module.exports = class Minesweeper extends events {
     if (typeof options.timeoutTime !== 'number') throw new TypeError('INVALID_TIME: Timeout time option must be a number.');
     if (typeof options.winMessage !== 'string') throw new TypeError('INVALID_MESSAGE: Win Message option must be a string.');
     if (typeof options.loseMessage !== 'string') throw new TypeError('INVALID_MESSAGE: Lose Message option must be a string.');
+    if (typeof options.timeoutMessage !== 'string') throw new TypeError('INVALID_MESSAGE: Timeout Message option must be a string.');
     if (options.mines < 1 || options.mines > 24) throw new RangeError('INVALID_MINES: mines option must be between 1 and 24.');
     if (options.playerOnlyMessage !== false) {
       if (!options.playerOnlyMessage) options.playerOnlyMessage = 'Only {player} can use these buttons.';
       if (typeof options.playerOnlyMessage !== 'string') throw new TypeError('INVALID_MESSAGE: playerOnly Message option must be a string.');
     }
 
-    
     super();
     this.options = options;
     this.message = options.message;
@@ -59,67 +56,66 @@ module.exports = class Minesweeper extends events {
     }
   }
 
-
   async sendMessage(content) {
-    if (this.options.isSlashGame) return await this.message.editReply(content);
-    else return await this.message.channel.send(content);
+    if (this.options.isSlashGame) return await this.message.editReply(content).catch(() => {});
+    else return await this.message.channel.send(content).catch(() => {});
   }
-
 
   async startGame() {
     if (this.options.isSlashGame || !this.message.author) {
-      if (!this.message.deferred) await this.message.deferReply().catch(e => {});
+      if (!this.message.deferred) await this.message.deferReply().catch(() => {});
       this.message.author = this.message.user;
       this.options.isSlashGame = true;
     }
     this.plantMines();
     this.showFirstBlock();
 
-
     const embed = new EmbedBuilder()
-    .setColor(this.options.embed.color)
-    .setTitle(this.options.embed.title)
-    .setDescription(this.options.embed.description)
-    .setAuthor({ name: this.message.author.tag, iconURL: this.message.author.displayAvatarURL({ dynamic: true }) });
+        .setColor(this.options.embed.color)
+        .setTitle(this.options.embed.title)
+        .setDescription(this.options.embed.description)
 
     const msg = await this.sendMessage({ embeds: [embed], components: this.getComponents() });
     return this.handleButtons(msg);
   }
 
-
   handleButtons(msg) {
+    if (!msg) return;
     const collector = msg.createMessageComponentCollector({ idle: this.options.timeoutTime });
 
-
     collector.on('collect', async btn => {
-      await btn.deferUpdate().catch(e => {});
       if (btn.user.id !== this.message.author.id) {
-        if (this.options.playerOnlyMessage) btn.followUp({ content: formatMessage(this.options, 'playerOnlyMessage'), ephemeral: true });
+        if (this.options.playerOnlyMessage) {
+          return btn.reply({ content: formatMessage(this.options, 'playerOnlyMessage'), ephemeral: true }).catch(() => {});
+        }
         return;
       }
+
+      await btn.deferUpdate().catch(() => {});
 
       const x = parseInt(btn.customId.split('_')[1]);
       const y = parseInt(btn.customId.split('_')[2]);
       const index = (y * this.length + x);
 
-      if (this.gameBoard[index] === true) return collector.stop();
-      const mines = this.getMinesAround(x, y);
-      this.gameBoard[index] = mines;
+      if (this.gameBoard[index] === true) return collector.stop('mine');
 
-      if (this.foundAllMines()) return collector.stop();
-      return await msg.edit({ components: this.getComponents() });
-    })
+      this.gameBoard[index] = this.getMinesAround(x, y);
 
+      if (this.foundAllMines()) return collector.stop('win');
+
+      return await btn.editReply({ components: this.getComponents() }).catch(() => {});
+    });
 
     collector.on('end', async (_, reason) => {
-      if (reason === 'user' || reason === 'idle') return this.gameOver(msg, this.foundAllMines());
-    })
+      if (['win', 'mine', 'user', 'idle'].includes(reason)) {
+        return this.gameOver(msg, this.foundAllMines(), reason === 'idle');
+      }
+    });
   }
 
-
-  gameOver(msg, result) {
+  gameOver(msg, result, isTimeout = false) {
     const MinesweeperGame = { player: this.message.author, blocksTurned: this.gameBoard.filter(Number.isInteger).length };
-    this.emit('gameOver', { result: (result ? 'win' : 'lose'), ...MinesweeperGame });
+    this.emit('gameOver', { result: isTimeout ? 'timeout' : (result ? 'win' : 'lose'), ...MinesweeperGame });
 
     for (let y = 0; y < this.length; y++) {
       for (let x = 0; x < this.length; x++) {
@@ -128,72 +124,68 @@ module.exports = class Minesweeper extends events {
       }
     }
 
-
     const embed = new EmbedBuilder()
-    .setColor(this.options.embed.color)
-    .setTitle(this.options.embed.title)
-    .setDescription(result ? this.options.winMessage : this.options.loseMessage)
-    .setAuthor({ name: this.message.author.tag, iconURL: this.message.author.displayAvatarURL({ dynamic: true }) });
+        .setColor(this.options.embed.color)
+        .setTitle(this.options.embed.title)
+        .setDescription(isTimeout ? this.options.timeoutMessage : (result ? this.options.winMessage : this.options.loseMessage))
+        .setAuthor({ name: this.message.author.tag, iconURL: this.message.author.displayAvatarURL({ dynamic: true }) });
 
-    return msg.edit({ embeds: [embed], components: disableButtons(this.getComponents(true, result)) });
+    return msg.edit({ embeds: [embed], components: disableButtons(this.getComponents(true, result)) }).catch(() => {});
   }
 
-
   plantMines() {
-    for (let i = 0; i <= this.options.mines; i++) {
-      const x = Math.floor(Math.random() * 5);
-      const y = Math.floor(Math.random() * 5);
+    let planted = 0;
+    while (planted < this.options.mines) {
+      const x = Math.floor(Math.random() * this.length);
+      const y = Math.floor(Math.random() * this.length);
       const index = (y * this.length + x);
 
-      if (this.gameBoard[index] !== true) this.gameBoard[index] = true;
-      else i = (i - 1);
+      if (this.gameBoard[index] !== true) {
+        this.gameBoard[index] = true;
+        planted++;
+      }
     }
   }
 
-
   getMinesAround(x, y) {
     let minesAround = 0;
-
-    for (let row = - 1; row < 2; row++) {
-      for (let col = - 1; col < 2; col++) {
-        const block = { x: x + col, y: y + row };
-        if (block.x < 0 || block.x >= 5 || block.y < 0 || block.y >= 5) continue;
+    for (let row = -1; row <= 1; row++) {
+      for (let col = -1; col <= 1; col++) {
         if (row === 0 && col === 0) continue;
-
-        if (this.gameBoard[block.y * this.length + block.x] === true) minesAround += 1;
+        const nx = x + col;
+        const ny = y + row;
+        if (nx >= 0 && nx < this.length && ny >= 0 && ny < this.length) {
+          if (this.gameBoard[ny * this.length + nx] === true) minesAround++;
+        }
       }
     }
     return minesAround;
   }
 
-
   showFirstBlock() {
-    const Blocks = [];
+    const emptyBlocks = [];
+    const allBlocks = [];
 
     for (let y = 0; y < this.length; y++) {
       for (let x = 0; x < this.length; x++) {
-        if (this.gameBoard[y * this.length + x] === true) Blocks.push({ x, y });
+        if (this.gameBoard[y * this.length + x] === false) {
+          allBlocks.push({ x, y });
+          if (this.getMinesAround(x, y) === 0) emptyBlocks.push({ x, y });
+        }
       }
     }
 
-    const emptyBlocks = Blocks.filter(b => !this.getMinesAround(b.x, b.y));
-    const blocks = emptyBlocks.length ? emptyBlocks : Blocks;
-
-    const rBlock = blocks[Math.floor(Math.random() * blocks.length)];
+    const pool = emptyBlocks.length ? emptyBlocks : allBlocks;
+    const rBlock = pool[Math.floor(Math.random() * pool.length)];
     this.gameBoard[rBlock.y * this.length + rBlock.x] = this.getMinesAround(rBlock.x, rBlock.y);
   }
 
-
   foundAllMines() {
-    let found = true;
-    for (let y = 0; y < this.length; y++) {
-      for (let x = 0; x < this.length; x++) {
-        if (this.gameBoard[y * this.length + x] === false) found = false;
-      }
+    for (let i = 0; i < this.gameBoard.length; i++) {
+      if (this.gameBoard[i] === false) return false;
     }
-    return found;
+    return true;
   }
-
 
   getComponents(showMines, found) {
     const components = [];
@@ -201,22 +193,39 @@ module.exports = class Minesweeper extends events {
     for (let y = 0; y < this.length; y++) {
       const row = new ActionRowBuilder();
       for (let x = 0; x < this.length; x++) {
+        const block = this.gameBoard[y * this.length + x];
 
-        const block = this.gameBoard[y * 5 + x];
-        const isNumber = getNumEmoji(block || null);
-        const displayMine = (block === true && showMines);
+        const isRevealed = typeof block === 'number';
+        const isNumber = isRevealed ? getNumEmoji(block) : null;
+        const displayMine = Boolean(block === true && showMines);
+
+        let style = 'PRIMARY';
+
+        if (displayMine) {
+          style = found ? 'SUCCESS' : 'DANGER';
+        } else if (isRevealed) {
+          style = 'SECONDARY';
+        }
 
         const btn = new ButtonBuilder()
-        .setStyle(displayMine ? (found ? 'SUCCESS' : 'DANGER') : (isNumber || block === 0 ? 'SECONDARY' : 'PRIMARY'))
-        .setCustomId('minesweeper_' + x + '_' + y)
+            .setStyle(style)
+            .setCustomId(`minesweeper_${x}_${y}`)
+            .setDisabled(isRevealed);
 
-        if (displayMine || isNumber) btn.setEmoji(displayMine ? (found ? this.emojis.flag : this.emojis.mine) : isNumber);
-        else btn.setLabel('\u200b');
+        if (displayMine) {
+          btn.setEmoji(found ? this.emojis.flag : this.emojis.mine);
+        } else if (isRevealed) {
+          if (isNumber) btn.setEmoji(isNumber);
+          else btn.setLabel('\u200b');
+        } else {
+          btn.setLabel('\u200b');
+        }
+
         row.addComponents(btn);
       }
       components.push(row);
     }
-    
+
     return components;
   }
 }
